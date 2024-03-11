2b8133e9b148bba9b4e9c8e8172996915f5d3df8
wei <alex20180614@gmail.com>
2024-03-11 10:57:55 -0400

    print data

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -20,3 +20,19 @@ class ProjectManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectManager
         fields = ("name", "id")
+
+#------------------------------------------------------------------------------------ for print data
+# print if is't suc   #  Project ， ID  1
+project_instance = Project.objects.get(
+    pk=1
+)  
+project_serializer = ProjectSerializer(instance=project_instance)
+print(project_serializer.data,"aa")
+
+
+#  ProjectManagerSerializer  #  ProjectManager instance，ID 1
+project_manager_instance = ProjectManager.objects.get(
+    pk=1
+)
+project_manager_serializer = ProjectManagerSerializer(instance=project_manager_instance)
+print(project_manager_serializer.data,"bb")
